95af36d789676f4497a1b3eab5bb667469ce5a02
Veer-Vamshi01 <146912286+vamshiVeer@users.noreply.github.com>
2024-01-29 16:35:35 +0530

    Avatar Card Completed
    Avatar Card Completed

---
+++ src/components/Core/Card.tsx
@@ -0,0 +1,68 @@
+// Card.tsx
+import React from "react";
+import { TouchableOpacity, View, Text, StyleSheet } from "react-native";
+import cn from "classnames";
+
+export interface CardProps {
+  title?: string;
+  children?: React.ReactNode;
+  onCardPress?: () => void;
+  headingBackgroundColor?: string;
+  headerTextColor?: string;
+  width?: string;
+}
+
+const Card: React.FC<CardProps> = ({
+  title,
+  children,
+  onCardPress = () => {},
+  headingBackgroundColor = "bg-white",
+  headerTextColor = "text-black",
+  width = "w-full",
+}) => {
+  const styles = StyleSheet.create({
+    card: {
+      backgroundColor: "white",
+      borderRadius: 8,
+      shadowColor: "#000",
+      shadowOffset: { width: 0, height: 2 },
+      shadowOpacity: 0.3,
+      shadowRadius: 4,
+      elevation: 5,
+      margin: 10,
+    },
+  });
+
+  return (
+    <TouchableOpacity onPress={onCardPress} activeOpacity={0.7}>
+      <View className={cn(` rounded-lg  m-2.5 ${width}`)} style={styles.card}>
+        <View
+          className={cn(
+            `rounded-t-lg p-2.5 justify-center items-center ${headingBackgroundColor}`
+          )}
+        >
+          {title && (
+            <Text className={cn(`text-lg font-bold mb-1.5 ${headerTextColor}`)}>
+              {title}
+            </Text>
+          )}
+        </View>
+        <View className="p-2.5">{children}</View>
+      </View>
+    </TouchableOpacity>
+  );
+};
+
+export default Card;
+
+//  <Card
+//    title="Card Title"
+//    headingBackgroundColor="bg-[#AC7D88]"
+//    width="w-[200]"
+//    headerTextColor="text-[#fff]"
+//  >
+//   inside content goes here
+// eg: <View>
+//     <Text>something</Text>
+// </View>
+//  </Card>;
--- src/screens/Home.screen.tsx
+++ src/screens/Home.screen.tsx
@@ -1,12 +1,15 @@
 import React, { useContext } from "react";
 import { Text, Button, View } from "react-native";
 import { ScrollView, TextInput } from "react-native-gesture-handler";
-import { BlockQuote } from "../components/Core/BlockQuote";
-import { Alert } from "../components/Core/Alert";
-import { UIContext } from "../store/context";
+
 import Container from "../components/Core/Container";
 import { Badge } from "../components/Core/Badge";
 import { DialogBox } from "../components/Core/DialogBox";
+import { BlockQuote } from "../components/Core/BlockQuote";
+import { Alert } from "../components/Core/Alert";
+import { UIContext } from "../store/context";
+import Card from "../components/Core/Card";
+import Avatar from "../components/Core/Avatar";
 
 const HomeScreen: React.FC = () => {
   const {
@@ -75,6 +78,65 @@ const HomeScreen: React.FC = () => {
           />
           <Text>This is the content of the dialog box.</Text>
         </DialogBox>
+      </Container>
+      <Button title="Show Prompt" onPress={() => updatePromptModalOpen(true)} />
+      <Alert
+        message="Enter a value:"
+        visible={promptModalOpen}
+        onClose={() => updatePromptModalOpen(false)}
+        variant="prompt"
+        // promptValue={promptValue}
+      />
+
+      <Container>
+        <Text className="font-bold text-lg">Avatar</Text>
+        <View className="items-center justify-center flex-row ">
+          <View className="flex-1 items-center justify-center">
+            <Avatar
+              size={70}
+              shape="circle"
+              onClick={() => {
+                console.log("Avathar Clicked");
+              }}
+            />
+            <Text className="pt-2"> Default</Text>
+          </View>
+          <View className="flex-1 items-center justify-center">
+            <Avatar
+              uri={require("../../assets/sampleprofilepic.png")}
+              size={70}
+              shape="circle"
+              onClick={() => {
+                console.log("Avathar Clicked");
+              }}
+            />
+            <Text className="pt-2"> Uploaded image </Text>
+          </View>
+        </View>
+      </Container>
+
+      <Container>
+        <Text className="font-bold text-lg">Card</Text>
+        <Card
+          title="Card Title"
+          headingBackgroundColor="bg-[#AC7D88]"
+          width="w-[170]"
+          headerTextColor="text-[#fff]"
+        >
+          <View className=" items-center justify-center">
+            <Avatar
+              uri={require("../../assets/sampleprofilepic.png")}
+              size={100}
+              shape="circle"
+              onClick={() => {
+                console.log("Avathar Clicked");
+              }}
+            />
+          </View>
+          <View className="py-4">
+            <Text>This is a text from Flex Component</Text>
+          </View>
+        </Card>
       </Container>
     </ScrollView>
   );
